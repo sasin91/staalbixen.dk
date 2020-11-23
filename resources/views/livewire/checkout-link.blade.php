73be28d3fdb66b56dda5da6870ad9bed58c4417d
Jonas <jonas.kerwin.hansen@gmail.com>
2020-11-23 16:36:31 +0100

    iOS standalone navigation

--- resources/views/livewire/checkout-link.blade.php
+++ resources/views/livewire/checkout-link.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ route('checkout') }}" type="button" class="inline-flex hover:text-orange">
+<button onclick="window.location = '{{ route('checkout') }}';" class="pointer-cursor inline-flex hover:text-orange">
     <x-heroicon-s-shopping-cart class="w-6 h-6" />
     {{ $this->totalCost }}
-</a>
+</button>
